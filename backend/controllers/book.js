fa981071ddd39544eec0207d8f2747022de1a6f8
Nubunduram <ceantoine@hotmail.fr>
2024-04-03 15:04:16 +0200

    fix: consistancy on promises

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -1,150 +1,134 @@
 const Book = require('../models/Book')
 const fs = require('fs');
 
-exports.getAllBooks = async (req, res, next) => {
-    try {
-        // Retrieves all books
-        const books = await Book.find()
-        res.status(200).json(books)
-    } catch (error) {
-        res.status(400).json({ error: error })
-    }
-}
+exports.getAllBooks = (req, res, next) => {
+    Book.find()
+        .then(books => {
+            res.status(200).json(books);
+        })
+        .catch(error => {
+            res.status(400).json({ error });
+        });
+};
 
-exports.getTopRatedBooks = async (req, res, next) => {
-    try {
-        // Retrieves the 3 top rated books
-        const topRatedBooks = await Book.find()
-            .sort({ averageRating: -1 })
-            .limit(3)
-        res.status(200).json(topRatedBooks)
-    } catch (error) {
-        res.status(500).json({ error: 'An error has occurred' })
-    }
-}
+exports.getTopRatedBooks = (req, res, next) => {
+    Book.find()
+        .sort({ averageRating: -1 })
+        .limit(3)
+        .then(topRatedBooks => {
+            res.status(200).json(topRatedBooks);
+        })
+        .catch(error => {
+            res.status(500).json({ error: 'An error has occurred' });
+        });
+};
 
-exports.getOneBook = async (req, res, next) => {
-    // Retrieves the book according to the id passed in the request
-    try {
-        const book = await Book.findOne({ _id: req.params.id })
-        if (book) {
-            res.status(200).json(book)
-        } else {
-            res.status(404).json({ error: 'Book not found' })
-        }
-    } catch (error) {
-        res.status(500).json({ error: error })
-    }
-}
+exports.getOneBook = (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+        .then(book => {
+            if (book) {
+                res.status(200).json(book);
+            } else {
+                res.status(404).json({ error: 'Book not found' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error });
+        });
+};
 
-exports.createBook = async (req, res, next) => {
-
-    // Convert JSON response to Object
-    const bookObject = JSON.parse(req.body.book)
-
-    // Checks that the request contains a file so as not to save an orphan file
+exports.createBook = (req, res, next) => {
+    const bookObject = JSON.parse(req.body.book);
     if (!req.file) {
-        return res.status(400).json({ message: 'File missing' })
-
+        return res.status(400).json({ message: 'File missing' });
     } else {
-        // Never trust user input
-        delete bookObject._id
-        delete bookObject._userId
+        delete bookObject._id;
+        delete bookObject._userId;
 
-        // If the user has not rated the book, empty the table (useful so that the user can still rate his book later)
         if (bookObject.ratings[0].grade === 0) {
-            bookObject.ratings = []
+            bookObject.ratings = [];
         }
 
-        const filename = req.file.filename
+        const filename = req.file.filename;
 
-        // Create a new Book from the reponse data
         const book = new Book({
             ...bookObject,
             userId: req.auth.userId,
             imageUrl: `${req.protocol}://${req.get('host')}/images/${filename}`
-        })
+        });
 
-        // Save the book to MongoDB
-        try {
-            await book.save()
-            res.status(201).json({ message: 'Book saved' })
-        } catch (error) {
-            fs.unlinkSync(`images/${filename}`)
-            res.status(400).json({ error })
-        }
+        book.save()
+            .then(() => {
+                res.status(201).json({ message: 'Book saved' });
+            })
+            .catch(error => {
+                fs.unlinkSync(`images/${filename}`);
+                res.status(400).json({ error });
+            });
     }
-}
+};
 
-exports.addBookRating = async (req, res, next) => {
-    // Check that the user has not already rated the book
-    const existingRating = await Book.findOne({
+exports.addBookRating = (req, res, next) => {
+    Book.findOne({
         _id: req.params.id,
         "ratings.userId": req.body.userId
     })
-    if (existingRating) {
-        return res.status(400).json({ message: 'User has already rated this book' })
-    }
-
-    // Check that the rating is a number between 0..5 included
-    if (!(req.body.rating >= 0) && !(req.body.rating <= 5) && (typeof req.body.rating === 'number')) {
-        return res.status(500).json({ message: 'Grade is not between 0 and 5 included or is not a number' })
-    }
-
-    try {
-        // Retrieves the book to rate according to the id of the request
-        const book = await Book.findOne({ _id: req.params.id })
-        if (!book) {
-            return res.status(404).json({ message: 'Book not found' })
-        }
-
-        // Add a new rating to the ratings array of the book
-        book.ratings.push({ userId: req.body.userId, grade: req.body.rating })
-
-        // Save the book to MongoDB, averageRating will be up to date on save
-        await book.save()
-        res.status(200).json(book)
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({ message: 'An error has occurred' })
-    }
-}
-
-exports.modifyBook = async (req, res, next) => {
-    // Check if a file is included in the request
-    try {
-        const bookObject = req.file
-            // If so, convert request JSON to Object
-            ? {
-                ...JSON.parse(req.body.book),
-                imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        .then(existingRating => {
+            if (existingRating) {
+                return res.status(400).json({ message: 'User has already rated this book' });
             }
-            // If not, use data from req.body
-            : { ...req.body }
 
-        delete bookObject._userId
+            if (!(req.body.rating >= 0) && !(req.body.rating <= 5) && (typeof req.body.rating === 'number')) {
+                return res.status(500).json({ message: 'Grade is not between 0 and 5 included or is not a number' });
+            }
 
-        // Retrieves book that match the id specified in the request params
-        const book = await Book.findOne({ _id: req.params.id })
+            Book.findOne({ _id: req.params.id })
+                .then(book => {
+                    if (!book) {
+                        return res.status(404).json({ message: 'Book not found' });
+                    }
 
-        // Check if user is authorized to modify the book
-        if (book.userId != req.auth.userId) {
-            return res.status(403).json({ message: 'Unauthorized request' })
-        }
+                    book.ratings.push({ userId: req.body.userId, grade: req.body.rating });
+                    return book.save();
+                })
+                .then(book => {
+                    res.status(200).json(book);
+                })
+                .catch(error => {
+                    console.error(error);
+                    res.status(500).json({ message: 'An error has occurred' });
+                });
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).json({ message: 'An error has occurred' });
+        });
+};
 
-        // If request contain a file, remove the old file from the back end (images folder)
-        if (req.file) {
-            const filename = book.imageUrl.split('/images/')[1]
-            fs.unlinkSync(`images/${filename}`)
-        }
+exports.modifyBook = (req, res, next) => {
+    const bookObject = req.file ? { ...JSON.parse(req.body.book), imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}` } : { ...req.body };
+    delete bookObject._userId;
 
-        // Update book
-        await Book.updateOne({ _id: req.params.id }, { ...bookObject, _id: req.params.id })
-        res.status(200).json({ message: 'Book modified!' })
-    } catch (error) {
-        res.status(400).json({ error })
-    }
-}
+    Book.findOne({ _id: req.params.id })
+        .then(book => {
+            if (book.userId != req.auth.userId) {
+                return res.status(403).json({ message: 'Unauthorized request' });
+            }
+
+            if (req.file) {
+                const filename = book.imageUrl.split('/images/')[1];
+                fs.unlinkSync(`images/${filename}`);
+            }
+
+            return Book.updateOne({ _id: req.params.id }, { ...bookObject, _id: req.params.id });
+        })
+        .then(() => {
+            res.status(200).json({ message: 'Book modified!' });
+        })
+        .catch(error => {
+            res.status(400).json({ error });
+        });
+};
 
 exports.deleteBook = (req, res, next) => {
     // Retrieves the book according to the id passed in the request
--- backend/middleware/sharp-config.js
+++ backend/middleware/sharp-config.js
@@ -1,28 +1,32 @@
 const sharp = require('sharp');
-const fs = require('fs');
+const fs = require('fs').promises;
 
-const resizeImage = async (req, res, next) => {
+const resizeImage = (req, res, next) => {
     if (req.file) {
-        try {
-            // Process image
-            const newFilename = req.file.filename.replace(/\.[^.]+$/, ".webp");
-            await sharp(req.file.path)
-                .resize({ width: 405, height: 568 })
-                .webp({ quality: 50 })
-                .toFile(`images/${newFilename}`);
+        // Process image
+        const newFilename = req.file.filename.replace(/\.[^.]+$/, ".webp");
 
-            // Delete the original image
-            fs.unlinkSync(req.file.path);
-
-            // add the new image
-            req.file.path = `images/${newFilename}`;
-            req.file.filename = newFilename;
-            req.file.mimetype = "image/webp";
-        } catch (error) {
-            return res.status(500).json({ message: 'Failed to resize image', error });
-        }
+        sharp(req.file.path)
+            .resize({ width: 405, height: 568 })
+            .webp({ quality: 50 })
+            .toFile(`images/${newFilename}`)
+            .then(() => {
+                // Delete the original image
+                return fs.unlink(req.file.path);
+            })
+            .then(() => {
+                // Add the new image
+                req.file.path = `images/${newFilename}`;
+                req.file.filename = newFilename;
+                req.file.mimetype = "image/webp";
+                next();
+            })
+            .catch(error => {
+                res.status(500).json({ message: 'Failed to resize image', error });
+            });
+    } else {
+        next();
     }
-    next();
 };
 
 module.exports = resizeImage;
