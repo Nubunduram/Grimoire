2b267cb36e9eefbd0e09d64c0bccfbd9c9ca5cd6
Nubunduram <ceantoine@hotmail.fr>
2024-04-19 12:48:15 +0200

    hosting back

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,17 @@
-const API_URL = 'http://localhost:4000';
+let API_URL = 'http://localhost:4000';
+
+if (process.env.NODE_ENV === 'production') {
+  API_URL = 'https://mon-vieux-grimoire-back-2984d53afe51.herokuapp.com';
+}
+
 export const API_ROUTES = {
   SIGN_UP: `${API_URL}/api/auth/signup`,
   SIGN_IN: `${API_URL}/api/auth/login`,
   BOOKS: `${API_URL}/api/books`,
   BEST_RATED: `${API_URL}/api/books/bestrating`,
 };
+
+// Other routes...
 
 export const APP_ROUTES = {
   SIGN_UP: '/Inscription',
